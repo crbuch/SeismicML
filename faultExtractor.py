c32452376fcd40b6ad493bcef2f98c93f8951179
CooperBuch <cbuch609@gmail.com>
2024-04-10 10:33:41 -0500

    added infer notebook

---
+++ faultExtractor.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch
+class FaultExtractor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=32, out_channels=1, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+        x = torch.relu(self.conv4(x))
+        return x
+        
